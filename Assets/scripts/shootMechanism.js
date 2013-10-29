ec6519f2039be499d6a7e502ceac62ba64770450
cristopherpf <cristopherpf@gmail.com>
2013-10-29 20:30:28 -0200

    Arrumando mecanismo de tiro

--- Assets/scripts/shootMechanism.js
+++ Assets/scripts/shootMechanism.js
@@ -1,37 +1,60 @@
 ﻿#pragma strict
-var prefabBullet:Rigidbody;
-var shootForce:float;
-var shootPosition:Transform;
+using UnityEngine;
 
-//Variaveis para a sincronizaçao
-private var lastSynchronizationTime:float = 0f;
-private var syncDelay:float = 0f;
-private var syncTime:float = 0f;
-private var syncStartPosition:Vector3 = Vector3.zero;
-private var syncEndPosition:Vector3 = Vector3.zero;
+using System.Collections;
 
-function Update(){
-	if (networkView.isMine) {
-		if(Input.GetButtonDown("Jump")) {
-			var instanceBullet = Instantiate(prefabBullet, shootPosition.position, shootPosition.rotation);
-			instanceBullet.rigidbody.AddForce(shootPosition.forward * shootForce);
-		}
-	}
-}
-
-function OnSerializeNetworkView(stream:BitStream, info:NetworkMessageInfo) {
-    var syncPosition:Vector3 = Vector3.zero;
-    if (stream.isWriting) {
-        syncPosition = rigidbody.position;
-        stream.Serialize(syncPosition);
-    } else {
-        stream.Serialize(syncPosition);
  
-        syncTime = 0f;
-        syncDelay = Time.time - lastSynchronizationTime;
-        lastSynchronizationTime = Time.time;
- 
-        syncStartPosition = rigidbody.position;
-        syncEndPosition = syncPosition;
+
+public class BasicShoot : MonoBehaviour
+
+{
+
+    public RaycastHit hit;
+
+    public int WeaponRange;
+
+    public GameObject GunParticle;
+
+    public AudioClip GunShotSound;
+
+    public Transform SpawnLocation;
+
+    
+
+    void Update()
+
+    {
+
+        if(Input.GetMouseButtonUp(0) && networkView.isMine)
+
+        {
+
+            networkView.RPC("Shoot", RPCMode.All);
+
+        };
+
     }
+
+    
+
+    [RPC]
+
+    void Shoot()
+
+    {
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(SpawnLocation.position, fwd, out hit, WeaponRange))
+
+        {
+
+            audio.PlayOneShot (GunShotSound);
+
+            Instantiate (GunParticle,hit.point,Quaternion.identity);
+
+        };
+
+    }
+
 }
