b27fdb5b31d9108efc3fe294479769f5fabbc86e
cristopherpf <cristopherpf@gmail.com>
2013-10-29 21:42:54 -0200

    Arrumando tiro

--- Assets/scripts/bulletCollision.js
+++ Assets/scripts/bulletCollision.js
@@ -1,13 +1,5 @@
 ﻿#pragma strict
 
 function OnCollisionEnter(collision : Collision) {
-		// Debug-draw all contact points and normals
-		//for (var contact : ContactPoint in collision.contacts) {
-		//	Debug.DrawRay(contact.point, contact.normal, Color.white);
-		//}
-		//
-		// Play a sound if the coliding objects had a big impact.		
-		//if (collision.relativeVelocity.magnitude > 2)
-		//	audio.Play();
 		Destroy (gameObject);
 }
--- Assets/scripts/shootMechanism.js
+++ Assets/scripts/shootMechanism.js
@@ -1,60 +1,17 @@
 ﻿#pragma strict
-using UnityEngine;
+var prefabBullet:Rigidbody;
+var shootPosition:Transform;
+var shootForce:float = 2000;
 
-using System.Collections;
-
- 
-
-public class BasicShoot : MonoBehaviour
-
-{
-
-    public RaycastHit hit;
-
-    public int WeaponRange;
-
-    public GameObject GunParticle;
-
-    public AudioClip GunShotSound;
-
-    public Transform SpawnLocation;
-
-    
-
-    void Update()
-
-    {
-
-        if(Input.GetMouseButtonUp(0) && networkView.isMine)
-
-        {
-
-            networkView.RPC("Shoot", RPCMode.All);
-
-        };
-
+function Update () {
+	if(Input.GetButtonDown("Jump") && networkView.isMine) {
+    	networkView.RPC("shoot", RPCMode.AllBuffered);
     }
+}
 
-    
-
-    [RPC]
-
-    void Shoot()
-
-    {
-
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-
-        if (Physics.Raycast(SpawnLocation.position, fwd, out hit, WeaponRange))
-
-        {
-
-            audio.PlayOneShot (GunShotSound);
-
-            Instantiate (GunParticle,hit.point,Quaternion.identity);
-
-        };
-
-    }
-
+@RPC
+function shoot() {
+     Debug.Log ("atirando em: " + transform.position + " , " + transform.rotation);
+     var instanceBullet = Instantiate(prefabBullet, shootPosition.position, shootPosition.rotation);
+     instanceBullet.rigidbody.AddForce(shootPosition.forward * shootForce);
 }
